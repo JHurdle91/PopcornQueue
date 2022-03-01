8376f22c7b1bcbb93be9c5a6f0587cad94aaebb4
Jesse Hurdle <Jesse.Hurdle@gmail.com>
2022-03-01 00:12:05 -0500

    implement top tab navigation for queue and other lists

--- src/infrastructure/navigation/queue.navigator.js
+++ src/infrastructure/navigation/queue.navigator.js
@@ -1,28 +1,80 @@
 import React from 'react';
-import {
-  CardStyleInterpolators,
-  createStackNavigator,
-} from '@react-navigation/stack';
+import { Ionicons } from '@expo/vector-icons';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
+import { LikesScreen } from '../../features/queue/screens/likes.screen';
 import { QueueScreen } from '../../features/queue/screens/queue.screen';
+import { theme } from '../theme';
 
-const QueueStack = createStackNavigator();
+const QueueTabs = createMaterialTopTabNavigator();
 
 export const QueueNavigator = () => {
   return (
-    <QueueStack.Navigator
-      headerMode="screen"
-      screenOptions={{
-        cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
-      }}
-    >
-      <QueueStack.Screen
+    <QueueTabs.Navigator initialRouteName="QueueTab">
+      <QueueTabs.Screen
+        name="QueueTab"
         options={{
-          header: () => null,
+          tabBarShowLabel: false,
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={focused ? 'list' : 'list-outline'}
+              size={24}
+              color={
+                focused ? theme.colors.brand.primary : theme.colors.ui.secondary
+              }
+            />
+          ),
         }}
-        name="Queue"
         component={QueueScreen}
       />
-    </QueueStack.Navigator>
+      <QueueTabs.Screen
+        name="LikesTab"
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={focused ? 'thumbs-up' : 'thumbs-up-outline'}
+              size={24}
+              color={
+                focused ? theme.colors.brand.primary : theme.colors.ui.secondary
+              }
+            />
+          ),
+        }}
+        component={LikesScreen}
+      />
+      <QueueTabs.Screen
+        name="DislikesTab"
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={focused ? 'thumbs-down' : 'thumbs-down-outline'}
+              size={24}
+              color={
+                focused ? theme.colors.brand.primary : theme.colors.ui.secondary
+              }
+            />
+          ),
+        }}
+        component={QueueScreen}
+      />
+      <QueueTabs.Screen
+        name="IgnoreTab"
+        options={{
+          tabBarShowLabel: false,
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={focused ? 'md-remove-circle' : 'md-remove-circle-outline'}
+              size={24}
+              color={
+                focused ? theme.colors.ui.error : theme.colors.ui.secondary
+              }
+            />
+          ),
+        }}
+        component={QueueScreen}
+      />
+    </QueueTabs.Navigator>
   );
 };
